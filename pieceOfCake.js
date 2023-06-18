106416baf114bccec36a9fcadd8a25917fd91659
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-06-18 20:37:09 +0900

    Update pieceOfCake.js

--- pieceOfCake.js
+++ pieceOfCake.js
@@ -16,3 +16,21 @@ switch(favoriteCake) {
   default:
     alert("I have never heard of that kind of cake before!");
 }
+
+
+/* The HTML File:
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script src="pieceOfCake.js"></script> 
+</body>
+</html>
+
+
+
+*/
